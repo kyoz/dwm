880e20b3a114ebbbe738a21748eb30181d2207e2
Kyoz <banminkyoz@gmail.com>
2020-09-16 17:37:03 +0700

    add(colors): purify

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Iosevka Nerd Font:size=11" };
+static const char dmenufont[]       = "Iosevka Nerd Font:size=14";
+static const char col_gray1[]       = "#282C34";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#BDBDBD";
+static const char col_gray4[]       = "#282C34";
+static const char col_cyan[]        = "#5FAFFF";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
