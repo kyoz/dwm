0c0dbc25abce07cde01f206e676839301694c67b
Kyoz <banminkyoz@gmail.com>
2020-09-26 17:39:40 +0700

    update key mappings

--- config.h
+++ config.h
@@ -46,8 +46,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]",      tile },    /* first entry is default */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -66,37 +66,43 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "vivaldi-stable", NULL };
+static const char *ibuscmd[]  = { "nextlang", NULL };
 
 #include "selfrestart.c"
 
 static Key keys[] = {
 	/* modifier                     key          function        argument */
-	{ MODKEY,                       XK_d,        spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return,   spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,        togglebar,      {0} },
-	{ MODKEY,                       XK_j,        focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,        focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_s,        swapfocus,      {.i = -1 } },
-	{ MODKEY,                       XK_r,        inplacerotate,  {.i = +1} },
-	{ MODKEY|ShiftMask,             XK_r,        inplacerotate,  {.i = +2} },
-	{ MODKEY,                       XK_i,        incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_u,        incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,        setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,        setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return,   zoom,           {0} },
-	{ MODKEY,                       XK_q,        killclient,     {0} },
-	{ MODKEY,                       XK_t,        setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,        setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,        setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,    setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,    togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,        togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,        view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,        tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,    focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period,   focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,    tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period,   tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_d,         spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,         spawn,          {.v = browsercmd } },
+  { MODKEY,                       XK_grave,     spawn,          {.v = ibuscmd } },
+	{ MODKEY,                       XK_b,         togglebar,      {0} },
+	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_s,         swapfocus,      {.i = -1 } },
+	{ MODKEY,                       XK_r,         inplacerotate,  {.i = +1} },
+	{ MODKEY|ShiftMask,             XK_r,         inplacerotate,  {.i = +2} },
+	{ MODKEY,                       XK_i,         incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_u,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return,    zoom,           {0} },
+	{ MODKEY,                       XK_q,         killclient,     {0} },
+	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,     setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,         togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,     tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,    tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_BackSpace, quit,           {0} },
+  { MODKEY|ShiftMask,             XK_F5,        self_restart,   {0} },
 	TAGKEYS(                        XK_1,                        0)
 	TAGKEYS(                        XK_2,                        1)
 	TAGKEYS(                        XK_3,                        2)
@@ -106,8 +112,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                        6)
 	TAGKEYS(                        XK_8,                        7)
 	TAGKEYS(                        XK_9,                        8)
-	{ MODKEY|ShiftMask,             XK_BackSpace, quit,          {0} },
-  { MODKEY|ShiftMask,             XK_F5,        self_restart,   {0} },
 };
 
 /* button definitions */
