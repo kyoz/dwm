0060ea13440dc4855fdcfc6e6ea70f8c91f1edb0
Kyoz <banminkyoz@gmail.com>
2020-09-28 09:12:13 +0700

    update config.h

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 12;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Nerd Font:size=11" };
@@ -13,6 +13,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#BDBDBD";
 static const char col_gray4[]       = "#282C34";
 static const char col_cyan[]        = "#5FAFFF";
+static const char col_pink[]        = "#FF79C6";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
