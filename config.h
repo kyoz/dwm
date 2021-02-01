bcdeb7833556e1eae4b619f2efaf7b0ee8c8cf0d
Kyoz <banminkyoz@gmail.com>
2021-02-01 16:35:34 +0700

    use brave as default browser

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "vivaldi-stable", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
 static const char *ibuscmd[]  = { "nextlang", NULL };
 
 #include "selfrestart.c"
